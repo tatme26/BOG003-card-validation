fb48991b1ccb6884fd0fff62081bfca1c0589c4d
Tatiana <tatme26.roka@gmail.com>
2021-06-29 22:18:24 -0500

    Creacion_homepage_html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+let boton = document.getElementById("miboton");
+boton.addEventListener("click",btnValidarTarjeta);
+
+function btnValidarTarjeta(){alert("Entró a btnValidarTarjeta")
+    if("click"){    
+document.getElementById("PagA").style.display="none";
+document.getElementById("PagB").style.display="inline";
+}
